c98e55f8db5a073a480b45fcf184b836687eec64
Alvin Joseph Arzadon <ajarzadon12@gmail.com>
2025-08-11 13:10:27 +0800

    Update webpack config

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,6 @@
 const { generateWebpackConfig, inliningCss } = require('shakapacker');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const isDevelopment = process.env.NODE_ENV !== 'production';
+const ReactRefreshWebpackPlugin = isDevelopment ? require('@pmmmwh/react-refresh-webpack-plugin') : null;
 
 const webpackConfig = generateWebpackConfig();
 
